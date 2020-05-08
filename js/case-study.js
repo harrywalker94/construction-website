b668ec01538023650d643b60be61564c50375d29
harry walker <harrywalker312@gmail.com>
2020-05-08 10:22:02 +0100

    finalising case studies

--- js/case-study.js
+++ js/case-study.js
@@ -3,6 +3,7 @@ const menu = [
         id: 1,
         title: "Heathrow",
         category: "Heathrow",
+        price: 15.99,
         img: "./assets/images/heathrow.jpg",
         desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
@@ -10,73 +11,128 @@ const menu = [
         id: 2,
         title: "Leicester",
         category: "Leicester",
+        price: 13.99,
         img: "./assets/images/leicester.jpg",
         desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
         id: 3,
-        title: "Cambridge",
-        category: "Cambridge",
-        img: "./assets/images/rosebury-av2.jpg",
+        title: "Hollis",
+        category: "Hollis",
+        price: 6.99,
+        img: "./assets/images/hollis.jpg",
         desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+        id: 4,
+        title: "London",
+        category: "London",
+        price: 13.99,
+        img: "./assets/images/leicester-plain.jpg",
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+        id: 5,
+        title: "Harlow",
+        category: "Harlow",
+        price: 13.99,
+        img: "./assets/images/hollis.jpg",
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     }
 ];
-
-const csSection = document.querySelector(".cs-section");
-const filterBtns = document.querySelector(".filter-btn");
-
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
-    displayCsItems(menu);
+
+    diplayMenuItems(menu);
+    displayMenuButtons(menu[0]);
+    btnContainer.parentNode.appendChild(btnContainer);
 });
 
-// filter items
-filterBtns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        console.log(e.currentTarget.dataset);
-    });
-});
+function diplayMenuItems(menuItems) {
 
-function displayCsItems(csItems) {
-    menu.splice(1);
-    let displayCs = csItems.map(function (item) { // map() takes each element from an original array, transforms it with a function that you specify, and adds the result to a new array in the same order.
+    let displayMenu = menuItems.map(function (item) {
+        // console.log(item);
+
         return ` <section class="cs-section">
-         <div class="cs-img">
-        <img src=${item.img} alt=${item.title}>
-        <div class="cs-header">
-            <h1 class="cs-title">Case Studies</h1>
-            <div class="cs-title-underline"></div>
-            <div class="scroll-down-div">
-                <a href="#case-study" class="scroll-down-arrow"><i class="fas fa-chevron-down"></i></a>
-            </div>
-        </div>
-    </div>
+        <div class="cs-img">
+       <img src=${item.img} alt=${item.title}>
+       <div class="cs-header">
+           <h1 class="cs-title">Case Studies</h1>
+           <div class="cs-title-underline"></div>
+       </div>
+   </div>
 
-    <main class="flex-container">
+   <main class="flex-container">
 
-        <section class="main-content blue">
-            <div id="corner"></div>
-            <div class="cs-box">
-                <div id="corner"></div>
+       <section class="main-content blue">
 
-                <h6>${item.title}</h6>
-                <p>${item.desc}</p>
-            </div>
-        </section>
+           <div class="cs-box">
+           
+           
+               <div id="corner"></div>
 
-        <aside class="sidebar green">
-            <h2>Sidebar</h2>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti temporibus recusandae nisi, ipsa esse
-                tempore aperiam laborum libero ipsum at? Accusamus nostrum, atque amet, fugiat voluptate iusto maiores
-                quam similique dolorum vel, voluptatum rerum dignissimos accusantium modi perferendis quidem in! Eum
-                velit aut voluptate ea rerum. Mollitia possimus aut itaque!
-            </p>
+               <h6>${item.title}</h6>
+               <p>${item.desc}</p>
+           </div>
+       </section>
 
-        </aside>
+       <aside class="sidebar green">
+           <h2>Sidebar</h2>
+           <p>
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti temporibus recusandae nisi, ipsa esse
+               tempore aperiam laborum libero ipsum at? Accusamus nostrum, atque amet, fugiat voluptate iusto maiores
+               quam similique dolorum vel, voluptatum rerum dignissimos accusantium modi perferendis quidem in! Eum
+               velit aut voluptate ea rerum. Mollitia possimus aut itaque!
+           </p>
 
-    </main>
-    </section>`;
+       </aside>
+
+   </main>
+   </section>`;
     });
-    displayCs = displayCs.join("");
-    csSection.innerHTML = displayCs;
+
+    // console.log(displayMenu);
+    sectionCenter.innerHTML = displayMenu[0];
+}
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        [""]
+    );
+    const categoryBtns = categories
+        .map(function (category) {
+            return `<button type="button" class="filter-btn" data-id=${category}><p class="filter-btn-text">${category}</p>
+          </button>`;
+        })
+        .splice(1).join("");
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    console.log(filterBtns);
+
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            // console.log(e.currentTarget.dataset);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function (menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if (category === "all") {
+                diplayMenuItems(menu);
+            } else {
+                diplayMenuItems(menuCategory);
+            }
+        });
+    });
 }
